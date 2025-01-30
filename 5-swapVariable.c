55d6706c2c3259b5510d4d8503ae643e3ec71efb
Jay Dhobi <138579588+jay3297@users.noreply.github.com>
2025-01-30 18:00:51 +0530

    Add files via upload
    Learn this concept of c programming conditionals ,loops ,type casting ,operators and solve this questation.

---
+++ 5-swapVariable.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+    int num1;
+    int num2;
+
+   
+    printf("\n enter the number1 :");
+    scanf("%d",&num1);
+    printf("\n enter the number2 :");
+    scanf("%d",&num2);
+    int swap =num1;
+    num1 =num2;
+    num2=swap;
+
+    
+    printf("\n num 1 :%d",num1);
+    printf("\n num 2 : %d",num2);
+
+}
---
+++ 9-vowels.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    for(int i=97;i<=122;i++)
+    {
+        if(i=='a'|| i=='e' || i=='i' || i=='o' || i=='u')
+        {
+            printf("\n print vowels : %c",i);
+        }
+    }
+}
