fac426e604cc894c4098c0d7731d7f71ab7ed148
jay3297 <jaydhobi31@gmail.com>
2025-02-03 16:55:17 +0530

    colum major update

--- 2D-Array/2-columnMajor.c
+++ 2D-Array/2-columnMajor.c
@@ -3,9 +3,9 @@ void main()
 {
     int arr[3][3];
     printf("\n enter the 2d array :");
-    for(int i=0;i<3;i++)
+    for(int j=0;j<3;j++)
     {
-        for(int j=0;j<3;j++)
+        for(int i=0;i<3;i++)
         {
             scanf("%d",&arr[i][j]);
         }
@@ -15,7 +15,7 @@ void main()
     {
         for(int i=0;i<3;i++)
         {
-            printf(" %d",arr[i][j]);
+            printf(" %d",arr[j][i]);
         }
         printf("\n");
     }
