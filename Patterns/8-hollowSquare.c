32be8bb2af2e3e05790ee7d4f21a8fa114d1a850
jay3297 <jaydhobi31@gmail.com>
2025-02-04 06:27:33 +0530

    hollow square pattern

---
+++ Patterns/8-hollowSquare.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf("enter n:");
+    scanf("%d",&n);
+    for(int row=0;row<n;row++)
+    {
+        for(int col=0;col<n;col++)
+        {
+            if(row==0 || row==n-1)
+            {
+                printf("*");
+            }
+            else{
+                if(col==0 || col==n-1)
+                {
+                    printf("*");
+                }
+                else{
+                    printf(" ");
+                }
+            }
+        }
+        printf("\n");
+    }
+}
